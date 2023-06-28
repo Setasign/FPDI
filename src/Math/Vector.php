f43f24a8c8f3ca92b532982444d2a37d45c75e9e
Jan Slabon <jan.slabon@setasign.com>
2023-06-28 14:57:31 +0200

    Create Vector.php

---
+++ src/Math/Vector.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of FPDI
+ *
+ * @package   setasign\Fpdi
+ * @copyright Copyright (c) 2023 Setasign GmbH & Co. KG (https://www.setasign.com)
+ * @license   http://opensource.org/licenses/mit-license The MIT License
+ */
+
+namespace setasign\Fpdi\Math;
+
+/**
+ * A simple 2D-Vector class
+ */
+class Vector
+{
+    protected $x;
+    protected $y;
+
+    public function __construct($x = .0, $y = .0)
+    {
+        $this->x = (float)$x;
+        $this->y = (float)$y;
+    }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    public function multiplyWithMatrix(Matrix $matrix)
+    {
+       list($a, $b, $c, $d, $e, $f) = $matrix->getValues();
+        $x = $a * $this->x + $c * $this->y + $e;
+        $y = $b * $this->x + $d * $this->y + $f;
+
+        return new self($x, $y);
+    }
+}
