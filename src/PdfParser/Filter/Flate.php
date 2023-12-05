52af4084547c2ea076b63fe59b32718be7de368f
Jan Slabon <jan.slabon@setasign.com>
2023-12-05 10:24:30 +0100

    Optimized fallback logic in flate decode method.

--- src/PdfParser/Filter/Flate.php
+++ src/PdfParser/Filter/Flate.php
@@ -55,21 +55,10 @@ class Flate implements FilterInterface
                     return $data;
                 }
 
-                // Try this fallback
-                $tries = 0;
+                // Try this fallback (remove the zlib stream header)
+                $data = @(gzinflate(substr($oData, 2)));
 
-                $oDataLen = strlen($oData);
-                while ($tries < 6 && ($data === false || (strlen($data) < ($oDataLen - $tries - 1)))) {
-                    $data = @(gzinflate(substr($oData, $tries)));
-                    $tries++;
-                }
-
-                // let's use this fallback only if the $data is longer than the original data
-                if (strlen($data) > ($oDataLen - $tries - 1)) {
-                    return $data;
-                }
-
-                if (!$data) {
+                if ($data === false) {
                     throw new FlateException(
                         'Error while decompressing stream.',
                         FlateException::DECOMPRESS_ERROR
