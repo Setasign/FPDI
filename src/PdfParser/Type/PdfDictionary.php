146cb0cf48015342f7407ef4cccb72c7b6497707
Jan Slabon <jan.slabon@setasign.com>
2025-01-31 11:20:24 +0100

    Reverted fix for implicitly nullable parameter declarations

--- src/PdfParser/Type/PdfDictionary.php
+++ src/PdfParser/Type/PdfDictionary.php
@@ -106,20 +106,15 @@ class PdfDictionary extends PdfType
      * @return PdfNull|PdfType
      * @throws PdfTypeException
      */
-    public static function get($dictionary, $key, $default = null)
+    public static function get($dictionary, $key, ?PdfType $default = null)
     {
-        if ($default !== null && !($default instanceof PdfType)) {
-            throw new \InvalidArgumentException('Default value must be an instance of PdfType or null');
-        }
         $dictionary = self::ensure($dictionary);
 
         if (isset($dictionary->value[$key])) {
             return $dictionary->value[$key];
         }
 
-        return $default === null
-            ? new PdfNull()
-            : $default;
+        return $default ?? new PdfNull();
     }
 
     /**
